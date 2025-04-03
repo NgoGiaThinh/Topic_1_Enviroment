94fa3611385710203e4de65fb6b603ab3eae2b55
Ngo Gia Thinh <ngogiathinh2511@gmail.com>
2025-04-03 09:44:24 +0700

    Implicit / Static Wait

--- selenium/webdriver/Topic_30_Wait_I_II.java
+++ selenium/webdriver/Topic_30_Wait_I_II.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,12 +13,12 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 
-public class Topic_30_Wait {
+public class Topic_30_Wait_I_II {
     WebDriver driver;
     Actions action;
     JavascriptExecutor jsExecutor;
 
-    public Topic_30_Wait() {
+    public Topic_30_Wait_I_II() {
     }
 
     @BeforeClass
---
+++ selenium/webdriver/Topic_31_Wait_III_Implicit.java
@@ -0,0 +1,107 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_31_Wait_III_Implicit {
+    WebDriver driver;
+    Actions action;
+    JavascriptExecutor jsExecutor;
+    public Topic_31_Wait_III_Implicit() {
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        this.driver = new FirefoxDriver();
+        //this.driver = new EdgeDriver();
+        //this.driver = new ChromeDriver();
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+
+        driver.manage().window().maximize();
+
+        action = new Actions(driver);
+        jsExecutor = (JavascriptExecutor) driver;
+    }
+
+    @Test(description = "thời gian implicit ngắn hơn element nó xuất hiện")
+    public void TC_01() {
+
+
+        //step 1: click vào button start
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        //loading icon sẽ xuất hiện trong vòng 5s
+
+        //step 2: HelloWorld text hiển thị = cần verify
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+
+
+    }
+
+    @Test(description = "thời gian implicit bằng thời gian element nó xuất hiện")
+    public void TC_02() {
+
+        //step 1: click vào button start
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        //loading icon sẽ xuất hiện trong vòng 5s
+
+
+        //step 2: HelloWorld text hiển thị = cần verify
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+    }
+
+    @Test(description = "thời gian implicit dài hơn element nó xuất hiện")
+    public void TC_03() {
+
+
+        //step 1: click vào button start
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        //loading icon sẽ xuất hiện trong vòng 5s
+
+        //step 2: HelloWorld text hiển thị = cần verify
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+    }
+
+
+    @Test(description = "thời gian implicit = 0")
+    public void TC_04() {
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+
+        //step 1: click vào button start
+        driver.findElement(By.cssSelector("div#start>button")).click();
+
+        //loading icon sẽ xuất hiện trong vòng 5s
+
+
+        //step 2: HelloWorld text hiển thị = cần verify
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#finish>h4")).getText(), "Hello World!");
+
+    }
+
+
+
+    @Test
+    public void TC_05() {
+        driver.get("");
+    }
+
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+}
